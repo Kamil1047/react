5d0d1f1f948daaa9648cdbb2d14aa6204c27bc39
indianic <indianic@ind952.local>
2021-08-09 13:03:29 +0530

    first commit

---
+++ src/components/UI/Card.js
@@ -0,0 +1,19 @@
+// import "./Card.css";
+
+// const Card = (props) => {
+//   const classes = "card" + props.className;
+
+//   return <div className={classes}>{props.children}</div>;
+// };
+
+// export default Card;
+
+import "./Card.css";
+
+const Card = (props) => {
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
